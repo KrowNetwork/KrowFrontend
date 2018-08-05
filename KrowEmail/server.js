a12dd2405d0d519e8d20e1c5474601fa7df1e716
unknown <siegel.tucker@gmail.com>
2018-08-04 23:56:31 -0400

    asdf

--- KrowEmail/server.js
+++ KrowEmail/server.js
@@ -8,9 +8,28 @@ var cognito = require("cognito-express")
 var fs = require('fs');
 const { exec } = require('child_process');
 var request = require('request');
+const PROD = false;
+const https = require('https');
 
+const lex = require('greenlock-express').create({
+    version: 'draft-11',
+    server: PROD ? 'https://acme-v02.api.letsencrypt.org/directory' : 'https://acme-staging-v02.api.letsencrypt.org/directory',
+    approveDomains: (opts, certs, cb) => {
+      if (certs) {
+        // change domain list here
+        opts.domains = ['krownetwork.com', 'api.krownetwork.com']
+      } else { 
+        // change default email to accept agreement
+        opts.email = 'tuckers@krow.network'; 
+        opts.agreeTos = true;
+      }
+      cb(null, { options: opts, certs: certs });
+    }
+    // optional: see "Note 3" at the end of the page
+    // communityMember: true
+  });
 
-var port = 80
+var port = 443
 
 const cognitoExpress = new cognito({
     region: "us-east-2",
@@ -305,7 +324,7 @@ app.post("/accept-hire", (req, res, next) => {
     res.send({"response": "success"})
 })
 
-app.listen(port, function (err) {
+https.createServer(lex.httpsOptions, app).listen(port, function (err) {
     if (err) {
       throw err
     }
