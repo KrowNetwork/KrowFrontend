b9c55cfff9609da4cb31c93ed1961b9c498fa1c4
unknown <siegel.tucker@gmail.com>
2018-11-17 09:56:45 -0500

    update

--- KrowEmail/server.js
+++ KrowEmail/server.js
@@ -128,6 +128,21 @@ app.use(function(req, res, next) {
         }
     })
   })
+//   https://us-18.api.mailchimp.com/3.0/lists/0d43791d4b
+  app.get("/g", (req, res, next) => {
+    var data = req.body
+    var auth = "Tucker:eac99e13e104235d60828809af71d173-us18"
+
+        request.post("https://us-18.api.mailchimp.com/3.0/lists/0d43791d4b", {headers: {"Authorization":auth}}, function(err, res2) {
+            if (err) {
+                // // console.log(err)
+                res.send(400, new Error(err));
+            } else {
+                res.send(200, res2.body)
+            }
+        })
+    }
+  
 
   app.get("/g", (req, res, next) => {
     var url = req.query.url
