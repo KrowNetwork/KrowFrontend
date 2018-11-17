cf594c5b6f0f493e75266591fd59788aba0308c6
unknown <siegel.tucker@gmail.com>
2018-11-17 10:02:49 -0500

    asdf

--- KrowEmail/server.js
+++ KrowEmail/server.js
@@ -129,11 +129,11 @@ app.use(function(req, res, next) {
     })
   })
 //   https://us-18.api.mailchimp.com/3.0/lists/0d43791d4b
-  app.get("/new-member", (req, res, next) => {
+  app.post("/new-member", (req, res, next) => {
     var data = req.body
     var auth = "Tucker:eac99e13e104235d60828809af71d173-us18"
 
-        request.post("https://us-18.api.mailchimp.com/3.0/lists/0d43791d4b", {headers: {"Authorization":auth}}, function(err, res2) {
+        request.post("https://us-18.api.mailchimp.com/3.0/lists/0d43791d4b", {headers: {"Authorization":auth}, json: data}, function(err, res2) {
             if (err) {
                 // // console.log(err)
                 res.send(400, new Error(err));
