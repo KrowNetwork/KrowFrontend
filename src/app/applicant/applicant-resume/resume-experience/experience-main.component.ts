9b62d2e680c20d1a8507051a912ec18a3c5a1e76
unknown <siegel.tucker@gmail.com>
2018-11-27 20:02:13 -0500

    update

--- src/app/applicant/applicant-resume/resume-experience/experience-main.component.ts
+++ src/app/applicant/applicant-resume/resume-experience/experience-main.component.ts
@@ -30,52 +30,63 @@ export class ExperienceMainComponent implements InterfaceComponent {
       this.closest(".social-edit").appendChild(self.deletedElements[0]);
       this.parentNode.removeChild(this);
     });
+    
     event.target.closest(".social-edit").replaceChild(element, event.target.closest("form"));
   }
 
   changeHandler(event){
-    event.target.closest("app-resume-experience").children[0].children[1].children[1].style = "display:show";
+    console.log(event.target)
+    console.log(event.target.closest("app-resume-experience"))
+    // event.target.closest("app-resume-experience").children[0].children[1].children[1].style = "display:show";
+    event.target.closest("app-resume-experience").children[0].children[1].children[1].setAttribute("style","display:show");
     // console.log(event)
   }
 
   changeCheck(event, data) {
     data.endDate = "1776-07"
-    event.target.closest("app-resume-experience").children[0].children[1].children[1].style = "display:show";
+    event.target.closest("app-resume-experience").children[0].children[1].children[1].setAttribute("style","display:show");
     console.log(data)
     // data.present = !data.present
     // console.log(data)
   }
 
-  submitHandler(event){
+  submitHandler(event, data){
     if(event.target.value == ""){
       return;
     }
-    console.log(event)
+    console.log(event.target.value)
+    // event.target.value = data.newskill
     this.createNew(event);
     event.target.value = "";
   }
 
   createNew(skill){
+    console.log(skill)
     // if(/\S/.test(skill.data.skill.toString())){
     //   // console.log("found something");
     //   return;
     // }
+
     var data = skill.target.value 
+    // console.log()
     skill.target.value = ""
-    var src = skill.path[1]
+    // var src = skill.path[1]
+    var src = skill.composedPath()[1]
     var node = document.createElement("li");
     var ul = src.closest(".tags");
+    
     // ul.appendChild(node)
     node.setAttribute("class", "addedTag");
     node.setAttribute("style", "margin-bottom: 5px; margin-top: 5px");
     // node.set
     console.log(node)
+    console.log("test")
     // var data = skill.data.skill.toString();
     var span = "<span class='tagRemove'>x</span>";
     var input = "<input type='hidden' name='tags[]' value='" + data + "'>";
     node.innerHTML = (data + span + input);
     node.children[0].addEventListener("click", function(){
-      this.closest(".resumeContainer").children[1].children[0].style = "margin-bottom: 15px; display: show";
+      this.closest(".resumeContainer").children[1].children[0].setAttribute("style", "margin-bottom: 15px; display: show");
       this.parentNode.remove();
     })
     // var ul = src.closest(".tags");
@@ -85,8 +96,8 @@ export class ExperienceMainComponent implements InterfaceComponent {
 
   removeSkill(event){
     // console.log("hi");
-    event.target.closest(".resumeContainer").children[1].children[0].style = "margin-bottom: 15px; display: show; color: #FFFFFF";
-    event.target.closest("app-resume-experience").children[0].children[1].children[1].style = "display:show";
+    event.target.closest(".resumeContainer").children[1].children[0].setAttribute("style", "margin-bottom: 15px; display: show; color: #FFFFFF");
+    event.target.closest("app-resume-experience").children[0].children[1].children[1].setAttribute("style","display:show");
     event.target.parentNode.remove();
   }
 }
