762d58d647cd39cebb34f9f275b18284e004da15
unknown <siegel.tucker@gmail.com>
2019-01-17 16:31:21 -0500

    Update server.js

--- KrowEmail/server.js
+++ KrowEmail/server.js
@@ -344,6 +344,34 @@ app.get("/get-employer-folders", (req, res, next) => {
     })
 })
 
+
+app.get("/get-employer-folder-base", (req, res, next) => {
+    var projectId = "krow-network-1533419444055"
+    const storage = new Storage({
+        projectId: projectId,
+    });
+
+    // var folder = req.query.folder
+    var id = req.query.id
+    // var filename = req.body.filename
+
+    // console.log(req.body)
+    // console.log(req.params)
+
+    const bucketName = 'employer-accounts';
+
+    var bucket = storage.bucket(bucketName)
+    results = []
+    bucket.getFiles({"prefix": id + "/" + folder + "/"}, function(err, files) {
+        files.forEach(f => {
+            if (f.name == "base.json") {
+                results.push(f)
+            }
+        })
+        res.status(200).send({results: results})
+    })
+})
+
 app.get("/create-employer-folder", (req, res, next) => {
     var projectId = "krow-network-1533419444055"
     const storage = new Storage({
