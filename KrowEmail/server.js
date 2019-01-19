e3a2d11daab3487008bfae67ec764415f07c68e4
unknown <siegel.tucker@gmail.com>
2019-01-19 15:29:36 -0500

    adf

--- KrowEmail/server.js
+++ KrowEmail/server.js
@@ -158,7 +158,7 @@ app.use(function(req, res, next) {
             console.log(f.name)
             if (f.name.endsWith(".json")) {
                 f.download(function(err, contents) {
-                    var document = JSON.parse(contents.toString()).responses[0]
+                    var document = JSON.parse(contents.toString()).responses[0].fullTextAnnotation
                     console.log(document)
                     document.pages.forEach(page => {
                         page.blocks.forEach(block => {
