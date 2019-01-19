b03518c573875f596956e69e5c157d06478a0ac9
unknown <siegel.tucker@gmail.com>
2019-01-19 14:59:08 -0500

    Update server.js

--- KrowEmail/server.js
+++ KrowEmail/server.js
@@ -135,7 +135,7 @@ app.use(function(req, res, next) {
     const filesResponse = await result.promise();
     console.log(filesResponse)
     console.log(filesResponse[2])
-    console.log(filesResponse[2].response)
+    console.log(filesResponse[2].response.value)
     // form.parse(req, async function (err, fields, files) {
     //     // console.log(err)
     //     // console.log(files)
